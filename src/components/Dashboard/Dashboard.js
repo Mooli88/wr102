8f27568f1640cae9258c9971d15497249e96aae3
Mooli M <moolipit@gmail.com>
2021-08-08 11:33:47 +0100

    style: add responsive styling to ComponentDetails

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,9 +15,7 @@ const Dashboard = () => {
       <div className={mainSection}>
         <ComponentTemp />
       </div>
-      <div>
-        <ComponentDetails />
-      </div>
+      <ComponentDetails />
     </div>
   );
 };
