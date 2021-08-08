63ec094f2e121f5d37306f9cf2e6ed600eb4955c
Mooli M <moolipit@gmail.com>
2021-08-08 02:14:04 +0100

    feat: finish basic implementation of weather temp

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -3,17 +3,13 @@ import PropTypes from "prop-types";
 import useWeather, { UNITS } from "../../hooks/useWeather";
 import SearchBar from "../SearchBar/SearchBar";
 import styles from "./WeatherDisplay.module.css";
+import Loading from "../Loading/Loading";
+
+const { root, weatherTemp, weatherTempVal, weatherCondition } = styles;
 
 const WeatherDisplay = (props) => {
-  const {
-    weatherState,
-    unitsState,
-    getWeatherByCityName,
-    changeUnits,
-    loadingState,
-  } = useWeather();
-  const units = unitsState === UNITS.METRIC ? "C" : "F";
-  const isMetricUnits = unitsState === UNITS.METRIC;
+  const { weatherState, getWeatherByCityName, changeUnits, loadingState } =
+    useWeather();
   const { weather, temp } = weatherState;
 
   useEffect(() => {
@@ -21,22 +17,19 @@ const WeatherDisplay = (props) => {
   }, []);
 
   return (
-    <div>
+    <div className={root}>
       {loadingState ? (
-        "Loading..."
+        <Loading />
       ) : (
-        <div>
-          <div>
+        <div className={weatherTemp}>
+          <div className={weatherCondition}>
             <img src={weather.icon} />
           </div>
-          <div>{weatherState.temp}&#186;</div>
+          <div className={`${weatherTempVal} text-bg-color--primary`}>
+            {temp.toFixed(1)}°
+          </div>
         </div>
       )}
-      <div onClick={() => changeUnits()}>
-        <span className={isMetricUnits && "active"}>C</span>
-        <span>/</span>
-        <span className={!isMetricUnits && "active"}>F</span>
-      </div>
       <SearchBar
         defaultValue="London"
         onSubmit={(val) => getWeatherByCityName({ city: val })}
