e1947ba435db60cfd2384f60e855370f980808e7
Mooli M <moolipit@gmail.com>
2021-08-08 02:17:05 +0100

    refactor: send all cpu info (not just main temp)

--- electron/utils/systemInfo.js
+++ electron/utils/systemInfo.js
@@ -1,17 +1,6 @@
 const si = require("systeminformation");
 
-async function getCpuTemperature() {
-  try {
-    si.cpuTemperature((val) => {});
-    const temp = await si.cpuTemperature();
-
-    return temp.main;
-  } catch (error) {
-    console.log("error " + error);
-  }
-}
-
 module.exports = {
   getCpuInfo: () => si.cpu(),
-  getCpuTemperature,
+  getCpuTemperature: () => si.cpuTemperature(),
 };
